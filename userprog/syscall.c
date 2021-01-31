fc6c86610ab6ebba2ebc20cd0e137ee82ddb6824
Luke Theodore Tchang <ltchang@myth66.stanford.edu>
2021-01-31 09:11:04 -0800

    added basic user memory checking function

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,10 +1,14 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+static void check_user_ptr (const void *usr_ptr);
+static void exit (int status);
 
 void
 syscall_init (void) 
@@ -17,4 +21,23 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+static void 
+check_user_ptr (const void *usr_ptr)
+{
+  if (usr_ptr == NULL) 
+    exit(-1);
+
+  if (!is_user_vaddr(usr_ptr))
+    exit(-1);
+  
+  uint32_t *pd = thread_current ()->pagedir;
+  if (pagedir_get_page (pd, usr_ptr) == NULL)
+    exit(-1);
+}
+
+static void 
+exit (int status)
+{
 }
