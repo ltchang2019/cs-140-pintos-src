6ac6da0033c503225457fcc72b89d043fc9d2f48
Luke Tchang <ltchang@stanford.edu>
2021-02-20 13:06:47 -0800

    prog: adds broken clock algorithm; 6/113 failing

--- vm/frame.c
+++ vm/frame.c
@@ -19,9 +19,12 @@ static struct frame_entry *frame_table_base;
 static struct frame_entry *lead_hand;
 static struct frame_entry *lag_hand;
 static size_t free_frames;
+static struct lock clock_lock;
 
 static struct frame_entry *page_kaddr_to_frame_addr (void *page_kaddr);
 static void *frame_evict_page (void);
+static struct frame_entry *clock_find_frame (void);
+static void clock_advance (void);
 
 /* Translates address returned by palloc_get_page() into
    address of corresponding frame in frame_table. */
@@ -64,6 +67,8 @@ frame_table_init (size_t num_frames)
       lock_init (&f->lock);
     }
 
+  lock_init (&clock_lock);
+
   /* Get base address of the user pool. */
   user_pool_base = palloc_get_user_pool_base ();
 
@@ -91,9 +96,8 @@ frame_alloc_page (enum palloc_flags flags, struct spte *spte)
 
   /* Get index to available frame and set fields in frame. */
   struct frame_entry *f = page_kaddr_to_frame_addr (page_kaddr);
-
+  f->page_kaddr = page_kaddr; // potentially redundant if evicted frame
   lock_acquire (&f->lock);
-  f->page_kaddr = page_kaddr;
   f->spte = spte;
   f->thread = thread_current ();
 
@@ -134,11 +138,64 @@ frame_free_page (void *page_kaddr)
   lock_acquire (&f->lock);
   pagedir_clear_page(thread_current ()->pagedir, f->spte->page_uaddr);
   f->page_kaddr = NULL;
-  f->spte = NULL;
   f->thread = NULL;
+  f->spte = NULL;
+  lock_release (&f->lock);
 
   palloc_free_page (page_kaddr);
-  lock_release (&f->lock);
+}
+
+static struct frame_entry *
+clock_find_frame (void)
+{
+  ASSERT (lock_held_by_current_thread (&clock_lock));
+
+  struct frame_entry *evict_frame;
+  while (true)
+    {
+      /* If frame was freed since clock algorithm began.
+         (i.e. we enter frame_evict because no page available,
+          get interrupted by another process that frees frame,
+          then see conditions below. */
+      if (lag_hand->thread == NULL && 
+          lock_try_acquire (&lag_hand->lock))
+        {
+          evict_frame = lag_hand;
+          break;
+        }  
+      
+      /* Frame is evictable. */
+      if (!pagedir_is_accessed (lag_hand->thread->pagedir,    
+                                lag_hand->spte->page_uaddr) &&
+          lock_try_acquire (&lag_hand->lock))
+        {
+          evict_frame = lag_hand;
+          break;
+        }
+
+      /* This condition is HACKY. */
+      if(lead_hand->thread != NULL)
+        pagedir_set_accessed (lead_hand->thread->pagedir, 
+                            lead_hand->spte->page_uaddr, false);
+      clock_advance ();
+    }
+  
+  /* Advance clock one more time so next frame_evict() call
+     doesn't evict just-allocated frame. */
+  clock_advance ();
+  return evict_frame;
+}
+
+static void
+clock_advance (void)
+{
+  ASSERT (lock_held_by_current_thread (&clock_lock));
+  
+  ++lead_hand, ++lag_hand;
+  if (lead_hand == frame_table_base + free_frames)
+    lead_hand = frame_table_base;
+  if (lag_hand == frame_table_base + free_frames)
+    lag_hand = frame_table_base;
 }
 
 /* Evict a page from it's frame and return the kernel virtual
@@ -146,29 +203,33 @@ frame_free_page (void *page_kaddr)
 static void *
 frame_evict_page (void)
 {
+  lock_acquire (&clock_lock);
+  struct frame_entry *f = clock_find_frame ();
+  ASSERT (lock_held_by_current_thread (&f->lock));
+  lock_release (&clock_lock);
+  
   /* CURRENTLY DOES NOT IMPLEMENT SECOND CHANCE CLOCK ALGORITHM. */
-  struct frame_entry *f = lead_hand;
+  // struct frame_entry *f = lead_hand;
   
   /* Increment leading hand for page eviction. */
-  lead_hand = lead_hand + 10;
-  if (lead_hand >= frame_table_base + free_frames)
-    lead_hand = frame_table_base;
+  // lead_hand = lead_hand + 10;
+  // if (lead_hand >= frame_table_base + free_frames)
+  //   lead_hand = frame_table_base;
 
-  lock_acquire (&f->lock);
+  // lock_acquire (&f->lock);
   struct thread *t = f->thread;
   struct spte *spte = f->spte;
-  void *upage = spte->page_uaddr;
 
   /* Write current page in frame to disk or swap if necessary. */
   if (spte->loc == SWAP ||
       spte->loc == STACK ||
-      (spte->loc == ZERO && pagedir_is_dirty (t->pagedir, upage)))
+      (spte->loc == ZERO && pagedir_is_dirty (t->pagedir, spte->page_uaddr)))
     {
       size_t swap_idx = swap_write_page (f->page_kaddr);
       spte->swap_idx = swap_idx;
       spte->loc = SWAP;
     }
-  else if (spte->loc == DISK && pagedir_is_dirty (t->pagedir, upage))
+  else if (spte->loc == DISK && pagedir_is_dirty (t->pagedir, spte->page_uaddr))
     {
       lock_acquire (&filesys_lock);
       file_write_at (spte->file, f->page_kaddr, spte->page_bytes, spte->ofs);
@@ -176,9 +237,11 @@ frame_evict_page (void)
     }
   
   /* Remove page mapping from owning thread to complete the eviction. */
-  pagedir_clear_page (t->pagedir, upage);
-  spte->loaded = false;
+  pagedir_clear_page (t->pagedir, spte->page_uaddr);
+  f->thread = NULL;
+  f->spte = NULL;
   lock_release (&f->lock);
+  spte->loaded = false;
 
   return f->page_kaddr;
 }
