a79a465b368a4e6c009a6c95cbb72d399a6f8754
Luke Tchang <ltchang@stanford.edu>
2021-03-11 20:26:06 -0800

    prog: dir-vine failing

--- filesys/cache.c
+++ filesys/cache.c
@@ -271,13 +271,13 @@ clock_find (void)
 
   while (true)
     {
-      printf ("CLOCK_FIND: try acquiring...\n");
+      // printf ("CLOCK_FIND: try acquiring...\n");
       if (rw_lock_shared_try_acquire (&lagging_hand->rw_lock))
         {
-          printf ("CLOCK_FIND: acquired. checking lagging hand dirty....\n");
+          // printf ("CLOCK_FIND: acquired. checking lagging hand dirty....\n");
           if (!lagging_hand->accessed && lagging_hand->rw_lock.active_readers < 2)
             {
-              printf ("Num Readers: %d\n", lagging_hand->rw_lock.active_readers);
+              // printf ("Num Readers: %d\n", lagging_hand->rw_lock.active_readers);
               rw_lock_shared_to_exclusive (&lagging_hand->rw_lock);
               size_t cache_idx = lagging_hand->cache_idx;
               
@@ -356,8 +356,15 @@ cache_find_block (block_sector_t sector)
          about sector_idx changing under us. */
       if (ce->sector_idx == sector)
         {
-          rw_lock_shared_acquire (&ce->rw_lock);
-          return ce->cache_idx;
+          while (!rw_lock_shared_try_acquire (&ce->rw_lock))
+            cond_wait (&eviction_cond, &eviction_lock);
+
+          /* If block still contains correct disk sector,
+             return given cache idx. Else, restart search. */
+          if (ce->sector_idx == sector)
+            return ce->cache_idx;
+          else
+            idx = 0;
         }
     }
 
@@ -377,33 +384,35 @@ cache_load (block_sector_t sector)
   size_t cache_idx;
   struct cache_entry *ce;
   
-  printf ("CACHE_LOAD: process %d waiting on eviction lock\n", thread_current ()->tid);
+  // printf ("CACHE_LOAD: process %d waiting on eviction lock\n", thread_current ()->tid);
   lock_acquire (&eviction_lock);
-  printf ("CACHE_LOAD: process %d GOT eviction lock\n", thread_current ()->tid);
+  // printf ("CACHE_LOAD: process %d GOT eviction lock\n", thread_current ()->tid);
 
   /* Block already in cache, so just return the cache_idx. */
-  printf ("CACHE_LOAD: process %d searching block\n", thread_current ()->tid);
+  // printf ("CACHE_LOAD: process %d searching block\n", thread_current ()->tid);
   cache_idx = cache_find_block (sector);
   if (cache_idx != BLOCK_NOT_PRESENT)
     {
-      
+      cond_broadcast (&eviction_cond, &eviction_lock);
       lock_release (&eviction_lock);
       // printf ("CACHE_LOAD: process %d RELEASING eviction lock\n", thread_current ()->tid);
       return cache_idx;
     }
 
-  printf ("CACHE_LOAD: process %d searching bitmap\n", thread_current ()->tid);
+  // printf ("CACHE_LOAD: process %d searching bitmap\n", thread_current ()->tid);
   /* Block not in cache, so find a free slot and load it in. */
   cache_idx = bitmap_scan_and_flip (cache_bitmap, 0, 1, false);
 
-  printf ("CACHE_LOAD: try to acquire bitmap block\n");
+  // printf ("CACHE_LOAD: try to acquire bitmap block\n");
   /* A free cache slot is available, so obtain the rw_lock on
      the cache_entry and set the sector_idx field. */
   ce = cache_metadata + cache_idx;
   if (cache_idx != BITMAP_ERROR && 
       rw_lock_shared_try_acquire (&ce->rw_lock))
     {
+      cond_broadcast (&eviction_cond, &eviction_lock);
       lock_release (&eviction_lock);
+
       void *cache_block_addr = cache_idx_to_cache_block_addr (cache_idx);
       block_read (fs_device, sector, cache_block_addr);
       return cache_idx;
@@ -416,7 +425,8 @@ cache_load (block_sector_t sector)
      be evicted by another process. */
   // printf ("EVICTING BLOCK\n");
   cache_idx = cache_evict_block ();
-  printf ("CACHE_LOAD: process %d RELEASING eviction lock\n", thread_current ()->tid);
+  // printf ("CACHE_LOAD: process %d RELEASING eviction lock\n", thread_current ()->tid);
+  cond_broadcast (&eviction_cond, &eviction_lock);
   lock_release (&eviction_lock);
 
   void *cache_block_addr = cache_idx_to_cache_block_addr (cache_idx);
