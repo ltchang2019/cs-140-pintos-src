54723373ed0b20deaac6e44e2a96edfc30375942
Joe Zhang <zz98@myth66.stanford.edu>
2021-03-11 22:54:35 -0800

    docs: finished pass; all tests passing

--- threads/synch.h
+++ threads/synch.h
@@ -45,10 +45,10 @@ struct lock
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
-bool lock_acquire_in_context (struct lock *lock);
+bool lock_acquire_in_context (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
-void lock_conditional_release (struct lock *lock, bool release);
+void lock_conditional_release (struct lock *, bool);
 bool lock_held_by_current_thread (const struct lock *);
 
 /* Condition variable. */
